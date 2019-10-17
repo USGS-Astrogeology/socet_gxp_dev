7e87ad12c302967aaa62df2752991991f6debaae
David P. Mayer <dpmayer@usgs.gov>
2019-10-17 14:09:11 -0700

    Clean up gpf2csv and add no-convert option to support projected CRS

--- Network_Utilities/gpf2csv.py
+++ Network_Utilities/gpf2csv.py
@@ -4,7 +4,7 @@ import sys
 import argparse
 import pandas as pd
 import numpy as np
-from plio.io.io_bae import read_gpf, save_gpf
+from plio.io.io_bae import read_gpf
 
 def parse_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -13,23 +13,24 @@ def parse_args():
                         help = "Input GPF to convert")
     parser.add_argument("outpath",
                         help = """Path to output file.""")
-    # parser.add_argument("--no-convert",
-    #                     action='store_false',
-    #                     help = "When set, the lat_Y_North and long_X_East fields will be copied as-is into output file. By default, these fields are converted from radians to degrees.")
+    parser.add_argument("--no-convert",
+                        action='store_true',
+                        help = "When set, the lat_Y_North and long_X_East fields will be copied as-is into output file. By default, these fields are assumed to be in radians, and are converted to degrees.")
     args = parser.parse_args()
     return args
 
 
-def main(gpf,outpath):
+def main(gpf,outpath,no_convert):
 
     df = read_gpf(gpf)
 
     columns = df.columns.values.tolist()
 
-    df['lat_Y_North'] = np.degrees(df['lat_Y_North'])
-    df['long_X_East'] = np.degrees(df['long_X_East'])
+    if not no_convert:
+        df['lat_Y_North'] = np.degrees(df['lat_Y_North'])
+        df['long_X_East'] = np.degrees(df['long_X_East'])
 
-    df.to_csv(path_or_buf=outpath,header=True,index=False,columns=list(df.columns))
+    df.to_csv(path_or_buf=outpath,header=True,index=False,columns=columns)
 
 
 
